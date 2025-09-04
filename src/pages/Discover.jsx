5ec647d8fac29eb0ea10a0c6ad1eec5025586024
Imanifari <imaniahweb@gmail.com>
2025-09-04 15:20:10 -0700

    Implement Discover page layout with genre selection

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,27 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SoundCard } from "../components";
+import { genres } from "../assets/constants";
+const Discover = () => {
+  const genreTitle = "pop";
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          value=""
+          onChange={() => {}}
+          className="bg-black text-genre-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
